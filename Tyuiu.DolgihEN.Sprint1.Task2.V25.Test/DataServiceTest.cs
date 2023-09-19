cf31554343189c57dbbf416788ac899c716f7f43
Tyuiu.DolgihEN@outlook.com <egordolgih02@list.ru>
2023-09-19 14:37:23 +0500

    Task2

---
+++ Tyuiu.DolgihEN.Sprint1.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DolgihEN.Sprint1.Task2.V25.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint1.Task2.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.ConvertRadsToDegrees(x);
+            Assert.AreEqual(57, 59, res);
+        }
+    }
+}
---
+++ Tyuiu.DolgihEN.Sprint1.Task2.V25/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DolgihEN.Sprint1.Task2.V25.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint1.Task2.V25
+{
+    //Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.
+    //Формулировка задания: Известен угол в радианах. Перевести угол в градусы.
+    //Что пользователь вводит? Угол в радианах (целое число)
+    //Что программа печатает на экране? Угол в градусах (вещественное число)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Долгих Е.Н | ИИПБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Долгих Егор Николаевич | ИИПБ-23-2                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Результат:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ConvertRadsToDegrees(x));
+            Console.ReadLine();
+        }
+    }
+}
