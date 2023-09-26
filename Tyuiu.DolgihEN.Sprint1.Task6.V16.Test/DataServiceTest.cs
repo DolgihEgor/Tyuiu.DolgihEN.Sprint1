a804269f95fa3e8effca6cc1c84826dcd9604f82
Tyuiu.DolgihEN@outlook.com <egordolgih02@list.ru>
2023-09-26 14:54:35 +0500

    Task6

---
+++ Tyuiu.DolgihEN.Sprint1.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DolgihEN.Sprint1.Task6.V16.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint1.Task6.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "Привет!?";
+            DataService ds = new DataService();
+            bool res = ds.CheckSpecSymbols(strTest);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DolgihEN.Sprint1.Task6.V16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DolgihEN.Sprint1.Task6.V16.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint1.Task6.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Долгих Е.Н | ИИПБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Долгих Егор Николаевич | ИИПБ-23-2                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* и проверяет что в строке есть восклицание (!) и вопрос (?).             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите текст:");
+            string value = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Результат:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Ваш текст:" + ds.CheckSpecSymbols(value));
+            Console.ReadLine();
+        }
+    }
+}
