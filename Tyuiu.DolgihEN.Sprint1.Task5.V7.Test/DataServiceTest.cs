9191bca5258df82ae630c3e21418ba98d8af6c25
Tyuiu.DolgihEN@outlook.com <egordolgih02@list.ru>
2023-09-26 13:38:16 +0500

    Task5

---
+++ Tyuiu.DolgihEN.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DolgihEN.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double f = 100;
+            DataService ds = new DataService();
+            double res = ds.AngleToHoursMinutes(f);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 3;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.DolgihEN.Sprint1.Task5.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DolgihEN.Sprint1.Task5.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DolgihEN.Sprint1.Task5.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e76d4481-f0ac-4bdf-988c-8cb06284c893")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
